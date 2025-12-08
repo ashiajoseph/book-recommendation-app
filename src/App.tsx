7e5add867540a5f9f039fa16f45966ddf7a45881
ashiajoseph <ashiajoseph@gmail.com>
2025-12-08 10:55:09 +0100

    Added route to pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/login'
 import PageNotFound from './pages/page-not-found'
 import WithAuth from './routes/with-authentication'
+import BookList from './pages/books'
+import BookDetails from './pages/books/book-details'
+
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login/>} />
-      <Route path="/books" element={<WithAuth><h2>Books</h2></WithAuth>} />
-      <Route path="/" element={<WithAuth><h2>Books</h2></WithAuth>}  />
+      <Route path="/books" element={<WithAuth><BookList /></WithAuth>} />
+      <Route path="/books/:bookId" element={<WithAuth><BookDetails /></WithAuth>} />
+      <Route path="/" element={<Navigate to="/login" replace />}  />
       <Route path="*" element={< PageNotFound/>} />
     </Routes>
   )
