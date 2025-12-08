e214544d1467ea68c7628a826a499aeb42ed5e2d
ashiajoseph <ashiajoseph@gmail.com>
2025-12-08 10:55:49 +0100

    Initialized axios and provided query and store.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import initializeAxios from './services/axios.ts';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 0,
+    },
+  },
+});
+
+initializeAxios();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 )
